f68983d7e9987692c96e7019e89841fb9dba159c
pierre thomas <pierre.thomas@essca.eu>
2021-03-10 14:53:21 +0100

    travail

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -23,13 +23,13 @@ class PiecesController < ApplicationController
     @piece = Piece.new(piece_params)
     @piece.user = current_user
 
-      if @piece.save
-        flash[:success] = "piece successfully created"
-        redirect_to piece_path(@piece)
-      else
-        flash[:error] = "Something went wrong"
-        render 'new'
-      end
+    if @piece.save
+      flash[:success] = "piece successfully created"
+      redirect_to piece_path(@piece)
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
   end
 
   def edit
