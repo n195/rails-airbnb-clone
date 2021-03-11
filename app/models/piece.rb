ea6e122c2d29bff346d7583dcc1c6f8550e651f3
Nona Schneider <nonaschneider99@gmail.com>
2021-03-11 11:14:49 +0100

    seed

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,4 @@
 class Piece < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   validates :name, :price, :creation_date, presence: :true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_141627) do
+ActiveRecord::Schema.define(version: 2021_03_10_135338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2021_03_10_141627) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "photo_url"
-    t.string "creator"
     t.index ["user_id"], name: "index_pieces_on_user_id"
   end
 
